d2e31c0313e7480ebfd5b5011c690387752f571b
uyenvo <uyend.vo@gmail.com>
2020-09-05 23:15:51 -0500

    new project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { CreativeComponent } from './creative/creative.component';
+import { DeveloperComponent } from './developer/developer.component';
+import { HomeComponent } from './home/home.component';
+
+const routes: Routes = [{
+  path: '',
+  component: HomeComponent,
+  children: [
+    {
+      path: 'creative',
+      component: CreativeComponent,
+    }, {
+      path: 'developer',
+      component: DeveloperComponent,
+    },
+  ],
+},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { CreativeComponent } from './creative/creative.component';
+import { DeveloperComponent } from './developer/developer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    CreativeComponent,
+    DeveloperComponent
   ],
   imports: [
     BrowserModule,
