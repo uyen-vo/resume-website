6948161f790d1dba1facfdeef4600cb3d6f08b8c
uyenvo <uyend.vo@gmail.com>
2020-09-05 23:52:17 -0500

    added blur/fade animations

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,14 +1,75 @@
 import { Component, OnInit } from '@angular/core';
+import { trigger, keyframes, style, animate, transition } from '@angular/animations';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
+  animations: [
+    trigger(
+      'inOutAnimation',
+      [
+        transition(
+          ':enter',
+          [
+            style({ height: 0, opacity: 0 }),
+            animate('.3s ease-out',
+                    style({ height: 300, opacity: 1 }))
+          ]
+        ),
+        transition(
+          ':leave',
+          [
+            style({ height: 300, opacity: 1 }),
+            animate('.3s ease-in',
+                    style({ height: 0, opacity: 0 }))
+          ]
+        )
+      ]
+    ),
+    trigger(
+      'slowIn',
+      [
+        transition(
+          ':enter',
+          [
+            style({ height: 0, opacity: 0 }),
+            animate('4s ease-in-out',
+                    style({ height: 300, opacity: 1 }))
+          ]
+        )
+      ]
+    ),
+    trigger('blur', [
+      transition(':enter', [
+        animate('1.5s ease-in-out', keyframes([
+
+          style({
+            filter: 'blur(1000px)',
+          }),
+
+          style({
+            filter: 'blur(0)',
+          })
+        ]))
+      ]),
+      transition(':enter', [
+        animate('2s ease-out', keyframes([
+          style({
+            filter: 'blur(0px)',
+          }),
+          style({
+            filter: 'blur(100px)',
+        })
+      ]))
+    ])
+  ]),
+  ]
 })
 export class HomeComponent implements OnInit {
 
   time = new Date();
-  currentPage: string;
+  currentPage = '';
 
   constructor() {
   }
@@ -19,5 +80,5 @@ export class HomeComponent implements OnInit {
       }, 1000);
   }
 
-  
+
 }
