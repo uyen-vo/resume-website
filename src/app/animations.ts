80b5f8f1cb2bdd3a01c84b4d489efa0e09accdfc
uyen-vo <uyend.vo@gmail.com>
2020-09-11 11:38:20 -0500

    staggered animations for Observables (2)

---
+++ src/app/animations.ts
@@ -0,0 +1,61 @@
+import
+{
+    animation, trigger, animateChild, group,
+    transition, animate, style, query, stagger
+} from '@angular/animations';
+
+export const transAnimation = animation([
+    style({
+        height: '{{ height }}',
+        opacity: '{{ opacity }}',
+        backgroundColor: '{{ backgroundColor }}'
+    }),
+    animate('{{ time }}')
+]);
+
+
+export const listStaggerCreative =
+    trigger('listStaggerCreative', [
+        transition('* <=> *', [
+            query(
+                ':enter',
+                [
+                    style({ opacity: 0, transform: 'translateY(-15px)' }),
+                    stagger(
+                        '.1s',
+                        animate(
+                            '.4s ease-in-out',
+                            style({ opacity: 1, transform: 'translateY(0px)' })
+                        )
+                    )
+                ],
+                { optional: true }
+            ),
+            query(':leave', animate('50ms', style({ opacity: 0 })), {
+                optional: true
+            })
+        ])
+    ]);
+
+export const listStaggerDev =
+    trigger('listStaggerDev', [
+        transition('* <=> *', [
+            query(
+                ':enter',
+                [
+                    style({ opacity: 0, transform: 'translateY(-15px)' }),
+                    stagger(
+                        '.3s',
+                        animate(
+                            '.5s ease-out',
+                            style({ opacity: 1, transform: 'translateY(0px)' })
+                        )
+                    )
+                ],
+                { optional: true }
+            ),
+            query(':leave', animate('50ms', style({ opacity: 0 })), {
+                optional: true
+            })
+        ])
+    ]);
