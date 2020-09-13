9e37dfe0fd711d1c5ae9803fa3089e24a93f7c99
Uyen Vo <uyend.vo@gmail.com>
2020-09-13 02:08:58 -0500

    begin lightbox implementation

--- src/app/lightbox/lightbox.component.ts
+++ src/app/lightbox/lightbox.component.ts
@@ -9,15 +9,40 @@ export class LightboxComponent implements OnInit {
 
   @Input() previews: string[];
   galleryToggle: boolean;
+  curImageSrc: string;
+  curIndex: number;
+  curImage: HTMLImageElement;
 
   constructor() { }
 
   ngOnInit(): void {
     this.galleryToggle = false;
+    this.curImageSrc = '/assets/smart-atm-1.JPG';
   }
 
-  openGallery(event: ElementRef): void {
+  openGallery(imgSrc: string, index: number): void {
     this.galleryToggle = true;
-    event.nativeElement.style.imageSrc 
+    this.curImageSrc = imgSrc;
+    this.curIndex = index;
+
+    this.swapCurImage();
   }
+
+  closeGallery(): void {
+    this.curImage.style.filter = 'brightness(50%)';
+    this.galleryToggle = false;
+  }
+  swapCurImage(): void {
+    this.curImage = <HTMLImageElement>document.getElementsByClassName("gallery-preview")[this.curIndex];
+
+    this.curImage.style.filter = 'brightness(100%)';
+  }
+
+  onImageClick(event: MouseEvent): void {
+    event.stopPropagation();
+
+
+  }
+
+
 }
