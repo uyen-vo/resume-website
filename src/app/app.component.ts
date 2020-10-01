de9b70453713b155258fec5f48f8c5f524bf1972
uyenvo <uyend.vo@gmail.com>
2020-10-01 02:54:21 -0500

    finished responsiveness for now

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'uyen-codes-website';
+
+  ngOnInit() {
+    //disable iOS bouncing?
+    // document.body.addEventListener('touchmove', function(e) {
+    //   e.preventDefault();
+    // }, { passive: false });
+  }
 }
